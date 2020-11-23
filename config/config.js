961f908d2b8b93cd566159729fca2a9540f75dd0
Shubhankar Saxena <saxena03121999@gmail.com>
2020-11-23 18:20:05 +0530

    Connected to MongoDb Atlas

--- config/config.js
+++ config/config.js
@@ -1,17 +1,17 @@
 const config={
     production :{
-        SECRET: process.env.SECRET,
-        DATABASE: process.env.MONGODB_URI
+        SECRET: 'mysecretkey',
+        DATABASE: 'mongodb+srv://handHoldingSolution:proudsrmite@123@cluster0.6jf9f.mongodb.net/annexure?retryWrites=true&w=majority'
     },
     default : {
         SECRET: 'mysecretkey',
-        DATABASE: 'mongodb://localhost:27017/Users'
+        DATABASE: 'mongodb+srv://handHoldingSolution:proudsrmite@123@cluster0.6jf9f.mongodb.net/annexure?retryWrites=true&w=majority'
     }
 }
 
 
 exports.get = function get(env){
-    console.log(config[env] || config.default)
+   // console.log(config[env] || config.default)
     return config[env] || config.default
     
 }
