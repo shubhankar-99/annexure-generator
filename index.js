3d4bce9da6e1ba886024d14f2a547e4fbbba66e8
Shubhankar Saxena <56796352+shubhankar-99@users.noreply.github.com>
2020-12-03 22:52:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 // app use
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "Client")));
+app.use('/static',express.static(path.join(__dirname, "Client")));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'Client', 'build', 'index.html'))
